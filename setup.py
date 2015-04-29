41ef2eae5e0f55570bf05b43c6404814367be16f
dkurkin <deemson.k@gmail.com>
2015-04-29 13:06:54 +0300

    README rst for PyPI added

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-import sys
 
-sys.path.insert(0, '.')
 version = __import__('pydto').__version__
 
-with open('README.md') as f:
+with open('README.rst') as f:
     long_description = f.read()
 
 description = long_description.splitlines()[0].strip()
@@ -17,7 +15,7 @@ setup(
     url='https://github.com/deemson/pydto',
     download_url='https://github.com/deemson/pydto',
     version=version,
-    description=description,
+    description='data validation and conversion library',
     long_description=long_description,
     license='MIT',
     platforms=['any'],
@@ -30,14 +28,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-    ],
-    install_requires=[
-        'setuptools >= 0.6b1',
-    ],
+    ]
 )
